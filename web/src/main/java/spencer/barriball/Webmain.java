8a2f1a46c69e639501ec5a1959a2840d73db23b0
5pence <sbarriball@me.com>
2019-04-21 23:11:36 +0100

    add Spring Web Module and skeleton frame

---
+++ web/src/main/java/spencer/barriball/Webmain.java
@@ -0,0 +1,12 @@
+package spencer.barriball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webmain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webmain.class, args);
+    }
+}
