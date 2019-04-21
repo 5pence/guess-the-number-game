f07de8599c3ea89c698633afd2390206faadf33e
5pence <sbarriball@me.com>
2019-04-21 23:51:19 +0100

    add Thymeleaf and home page initial template

---
+++ web/src/main/java/spencer/barriball/util/ViewNames.java
@@ -0,0 +1,10 @@
+package spencer.barriball.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames() {}
+}
