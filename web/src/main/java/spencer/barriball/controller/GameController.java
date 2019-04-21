842ec46f7158a2737690dc30fcc86c16e8fd862c
5pence <sbarriball@me.com>
2019-04-22 00:49:23 +0100

    add thymeleaf play template and mappings for game

---
+++ web/src/main/java/spencer/barriball/controller/GameController.java
@@ -0,0 +1,46 @@
+package spencer.barriball.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spencer.barriball.service.GameService;
+import spencer.barriball.util.AttributeNames;
+import spencer.barriball.util.GameMappings;
+import spencer.barriball.util.ViewNames;
+
+@Slf4j
+@Controller
+public class GameController {
+
+    // == fields ==
+    private final GameService gameService;
+
+    // == constructor ==
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    // == request methods ==
+    @GetMapping(GameMappings.PLAY)
+    public String play(Model model) {
+        model.addAttribute(AttributeNames.MAIN_MESSAGE, gameService.getMainMessage());
+        model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMessage());
+        log.info("inside play(), model = {}", model);
+        if (gameService.isGameOver()) {
+            return ViewNames.GAME_OVER;
+        }
+        return ViewNames.PLAY;
+    }
+
+    @PostMapping(GameMappings.PLAY)
+    public String processMessage(@RequestParam int guess) {
+        log.info("inside processMessage(), guess = {}", guess);
+        gameService.checkGuess(guess);
+        return GameMappings.REDIECT_PLAY;
+    }
+}
---
+++ web/src/main/java/spencer/barriball/util/GameMappings.java
@@ -0,0 +1,11 @@
+package spencer.barriball.util;
+
+public final class GameMappings {
+
+    // == constants ==
+    public static final String PLAY = "play";
+    public static final String REDIECT_PLAY = "redirect:/" + PLAY;
+
+    // == constructor ==
+    private GameMappings() {}
+}
